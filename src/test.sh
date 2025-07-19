4efdbf65d2759c271f3c9c16fb09f46c45fb0747
ayoubm-dev <ayoubmypc@gmail.com>
2025-07-19 15:53:16 +0100

    update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,11 @@
 EXPECTED="Hello Ayoub"
 
-OUTPUT=$(node -e "console.log(require('./src/app')('Ayoub')))
+OUTPUT=$(node -e "console.log(require('./src/app')('Ayoub'))")
 
-if ["$EXPECTED" = "$OUTPUT"]; then
-
-echo "✅ Test passed"
-
-exit 0
-
-else 
-echo "❌ Test failed! expected '$EXPECTED' but got this '$OUTPUT'"
-
-exit 1
-
+if [ "$EXPECTED" = "$OUTPUT" ]; then
+  echo "✅ Test passed"
+  exit 0
+else
+  echo "❌ Test failed! Expected '$EXPECTED' but got this '$OUTPUT'"
+  exit 1
 fi
